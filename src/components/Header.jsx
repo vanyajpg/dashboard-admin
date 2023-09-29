e426754679bd761c544161b5f3a86a37dc37408a
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-28 22:44:27 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography, Box, useTheme } from '@mui/material'
+import { tokens } from '../theme'
+
+const Header = ({title, subtitle}) =>  {
+    const theme = useTheme()
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
